e39edb4492b80c364d6ebf7c88c5929063d69622
crlt18 <caidenreece12@icloud.com>
2025-06-04 16:12:45 +0100

    basic player controller
    jump, running and sprinting covered

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,16 +5,17 @@ using UnityEngine.InputSystem;
 public class PlayerInput : MonoBehaviour
 {
 
-    [SerializeField] private float playerSpeed;
+    private float playerSpeed;
+    [SerializeField] private float originalSpeed;
     private float currentSpeed;
     [SerializeField] private float jumpForce;
     private Rigidbody2D rb;
-
-
+    float deadZone = 0.05f; //prevent flickering (player changing direction for a frame when coming to a stop)
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerSpeed = originalSpeed;
     }
 
     private void FixedUpdate()
@@ -27,16 +28,44 @@ public class PlayerInput : MonoBehaviour
         {
             rb.linearVelocity = new Vector2(playerSpeed, rb.linearVelocity.y);
         }
+        else
+        {
+            //if there is no input reduce sliding from momentum
+            float friction = 10f;  
+            float newVelX = Mathf.MoveTowards(rb.linearVelocityX, 0, friction * Time.fixedDeltaTime);
+            rb.linearVelocity = new Vector2(newVelX, rb.linearVelocity.y);
+        }
 
         if (Input.GetKey(KeyCode.Space))
         {
             rb.linearVelocity = new Vector2(currentSpeed, jumpForce);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            playerSpeed = originalSpeed * 1.3f;
+        }
+        else
+        {
+            playerSpeed = originalSpeed;
         }
     }
 
     private void Update()
     {
         currentSpeed = rb.linearVelocityX;
+
+        //logic to flip the player sprite depending on direction
+        Vector3 localScale = transform.localScale;
+        if (currentSpeed < -deadZone)
+        {
+            localScale.x = -Mathf.Abs(localScale.x);
+        }
+        else if (currentSpeed > deadZone) 
+        {
+            localScale.x = Mathf.Abs(localScale.x);
+        }
+        transform.localScale = localScale;
     }
 
 }
